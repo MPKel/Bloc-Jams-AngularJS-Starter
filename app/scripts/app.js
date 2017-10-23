ed9bca424917757453456e678ad8c314b65f9a1f
Michael <mpkelley08@gmail.com>
2017-10-23 14:59:13 -0400

    assignment 5 blocjams controllers: completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,7 @@
       })
       .state('album', {
         url: '/album',
+        controller: 'AlbumCtrl as albumSongs',
         templateUrl: '/templates/album.html'
       })
       .state('collection', {
