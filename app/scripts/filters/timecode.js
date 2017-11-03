4aadf4ca162db785ff5276796f8391a87f846c95
Michael <mpkelley08@gmail.com>
2017-11-03 14:36:54 -0400

    assignment 11 blocjams angular filters: completed

--- app/scripts/filters/timecode.js
+++ app/scripts/filters/timecode.js
@@ -1,5 +1,5 @@
 (function() {
-  function timecode() {
+  function timecode(SongPlayer) {
     return function(seconds){
       var seconds = Number.parseFloat(seconds);
 
@@ -7,18 +7,7 @@
         return '-:--';
       }
 
-
-      var wholeSeconds = Math.floor(seconds);
-      var minutes = Math.floor(wholeSeconds / 60);
-      var remainingSeconds = wholeSeconds % 60;
-
-      var output = minutes + ':';
-
-      if(remainingSeconds < 10){
-        output += '0';
-      }
-
-      output += remainingSeconds;
+      var output = buzz.toTimer(seconds);
 
       return output;
     };
@@ -26,5 +15,5 @@
 
   angular
     .module('blocJams')
-    .filter('timecode', timecode);
+    .filter('timecode', ['SongPlayer', timecode]);
   }) ();
